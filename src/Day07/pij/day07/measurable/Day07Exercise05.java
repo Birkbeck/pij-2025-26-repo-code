bc75508cc5399770a7b675beaeeecd4820e01a09
Carsten Fuhs <fuhs@informatik.rwth-aachen.de>
2025-10-27 15:20:37 +0000

    Day 7 code for Exercise 5

---
+++ src/Day07/pij/day07/measurable/Day07Exercise05.java
@@ -0,0 +1,18 @@
+package pij.day07.measurable;
+
+import pij.day06.point.Point;
+
+public class Day07Exercise05 {
+    public static void main(String[] args) {
+        // code commented out to allow for looking at it without the
+        // IDE's assessment whether it compiles
+        /*
+        Point p = new Point(3, 4);
+        System.out.println( p.getMeasure() );
+        System.out.println( p.getX() );
+        Measurable m = p;
+        System.out.println( m.getMeasure() );
+        System.out.println( m.getX() );
+         */
+    }
+}
