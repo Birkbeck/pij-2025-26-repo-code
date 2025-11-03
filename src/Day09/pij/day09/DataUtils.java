8cbc1e712dfd5dd3be167078a2dfb99b2246867f
Stealthness <sa.west@bbk.ac.uk>
2025-11-03 15:59:38 +0000

    Create DataUtils.java

---
+++ src/Day09/pij/day09/DataUtils.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class DataUtils {
+
+    static List<String> getCities(){
+        return new ArrayList<>(Arrays.asList("London", "Cardiff", "Rome", "Birmingham", "Glasgow",
+                "Belfast", "Dublin", "Madrid", "Oslo", "Warsaw", "Moscow", "Stockholm", "Bonn", "Berlin", "Paris",
+                "Vienna", "Athens"));
+    }
+
+    static String[] getNames(){
+        return new String[]{"Amy", "Bob", "Eddy", "Dave", "Fred", "Mary", "Jane", "Mo", "Julie", "abby", "Bo", "Roger",
+        "peter", "Greg", "Steve", "Stu", "Richard", "Jules", "Dee", "Imran", "Ingrid", "Santos", "Raj", "patty", "Eve",
+        "Tan", "Stan", "George", "Jerry", "Gustav", "Wendy", "Zoe", "Dan", "Doc", "Po", "Daz", "Cat", "ezzy", "Pru"};
+    }
+
+
+
+    static boolean isFirstLetterE(java.lang.String s){
+        return switch (s.charAt(0)){
+            case 'e', 'E' -> true;
+            default -> false;
+        };
+
+    }
+
+    public static <String> int eSort(String s1, String s2) {
+        if ((isFirstLetterE((java.lang.String) s1) && isFirstLetterE((java.lang.String) s2)) || (!isFirstLetterE((java.lang.String) s1) && !isFirstLetterE((java.lang.String) s2) ) ){
+            return 0;
+        } else if ((isFirstLetterE((java.lang.String) s1) && !isFirstLetterE((java.lang.String) s2))){
+            return -1;
+        }else{
+            return 1;
+        }
+    }
+}
