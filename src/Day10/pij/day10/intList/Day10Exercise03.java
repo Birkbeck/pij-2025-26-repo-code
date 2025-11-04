28757283e0d14ab6cc3f335d555fd12d2a1408d2
Carsten Fuhs <fuhs@informatik.rwth-aachen.de>
2025-11-04 21:07:26 +0000

    Day 10 code for exercises

---
+++ src/Day10/pij/day10/intList/Day10Exercise03.java
@@ -0,0 +1,42 @@
+package pij.day10.intList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day10Exercise03 {
+    public void launch(int userInput) {
+        List<Integer> intList = new ArrayList<Integer>();
+        intList.add(1);
+        intList.add(2);
+        intList.add(3);
+        intList.add(4);
+        intList.add(5);
+        intList.add(6);
+        try {
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+            intList.remove(0);
+            System.out.println(intList.get(userInput));
+        } catch (IndexOutOfBoundsException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Day10Exercise03 launcher = new Day10Exercise03();
+        //launcher.launch(0);
+        //launcher.launch(2);
+        //launcher.launch(4);
+        //launcher.launch(6);
+    }
+}
