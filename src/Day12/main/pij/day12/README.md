b5ce482ff93197590a7b4bd2be31642e1842b4b7
Carsten Fuhs <fuhs@informatik.rwth-aachen.de>
2025-11-12 11:08:48 +0000

    Day 12 code for exercises, with README.md

---
+++ src/Day12/main/pij/day12/README.md
@@ -0,0 +1,14 @@
+# Day 12 Repo Code
+
+The following directories relate to
+
+## Reading material and slides
+
++ person
+
+## Worksheet 
+
++ Q1 person
++ Q2 fibonacci
++ Q3 stringMaker
++ Q5 cashRegister
---
+++ src/Day12/main/pij/day12/stringMaker/StringMaker.java
@@ -0,0 +1,11 @@
+package pij.day12.stringMaker;
+
+public class StringMaker {
+    public static String build(int max) {
+        String s = "";
+        for (int i = 0; i < max; i++) {
+            s += i;
+        }
+        return s;
+    }
+}
