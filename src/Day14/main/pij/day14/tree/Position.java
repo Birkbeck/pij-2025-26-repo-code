4c0f0bcff813202502c043b062e2712ed44b4691
Carsten Fuhs <fuhs@informatik.rwth-aachen.de>
2025-11-17 22:33:56 +0000

    Day 14 code for reading material

---
+++ src/Day14/main/pij/day14/tree/Position.java
@@ -0,0 +1,117 @@
+package pij.day14.tree;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+
+/**
+ * A position describes a particular location in a tree via a sequence
+ * of Directions from the root. In other words, a position is given by
+ * a path from the root.
+ * <p>
+ * For instance, a position [RIGHT, LEFT] is reached from the root
+ * of a tree we first need to go to the right subtree and then to the
+ * left subtree. So for the tree
+ * <pre>
+ *
+ *     1
+ *    / \
+ *   2   3
+ *      / \
+ *     4   5
+ *
+ * </pre>
+ * the value at position [RIGHT, LEFT] is 4.
+ */
+public class Position {
+    /** The list of directions to represent a position in a tree,
+     *  where a low index indicates that the node is close to the
+     *  root of the tree. May neither be nor contain null.
+     */
+    private final List<Direction> directions;
+
+    /**
+     * Constructs an empty Position (with size 0), corresponding to the root
+     * of a Tree.
+     */
+    public Position() {
+        this(Collections.emptyList());
+    }
+
+    /**
+     * Constructs a Position corresponding to directions.
+     *
+     * @param directions indicates the Directions this Position will represent;
+     *                   may be modified after the constructor call without
+     *                   affecting this Position
+     * @throws NullPointerException if directions is null or contains null
+     */
+    public Position(List<Direction> directions) {
+        sanityCheck(directions);
+        this.directions = new ArrayList<>(directions);
+    }
+
+    private static void sanityCheck(List<Direction> directions) {
+        for (Direction d : directions) {
+            Objects.requireNonNull(d, "Direction must not be null!");
+        }
+    }
+
+    /**
+     * @return the number of Directions in this Position
+     */
+    public int size() {
+        return this.directions.size();
+    }
+
+    /**
+     * @param index we want the Direction indicated for the index;
+     *              0 <= i < this.size() must hold
+     * @return the Direction indicated at index i
+     */
+    public Direction get(int index) {
+        return this.directions.get(index);
+    }
+
+    /**
+     * Creates a Position for the suffix of this Position starting at fromIndex.
+     *
+     * @param fromIndex the starting index for the new Position
+     * @return a Position for the suffix of this Position starting at fromIndex
+     * @throws IllegalArgumentException if fromIndex < 0 or fromIndex > this.size()
+     */
+    public Position createPositionFrom(int fromIndex) {
+        if (fromIndex < 0 || fromIndex > this.size()) {
+            throw new IllegalArgumentException();
+        }
+        return new Position(this.directions.subList(fromIndex, this.directions.size()));
+    }
+
+    /**
+     * @return a String representation of this Position
+     */
+    @Override
+    public String toString() {
+        return this.directions.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+        Position position = (Position) o;
+        return Objects.equals(this.directions, position.directions);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(this.directions);
+    }
+
+}
---
+++ src/Day14/main/pij/day14/tree/TreeNode.java
@@ -0,0 +1,110 @@
+package pij.day14.tree;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * A node for a generic binary tree with a data element and
+ * two pointers to the left and right child node. Class has
+ * package-private visibility to indicate that it is solely
+ * for internal use in the package.
+ *
+ * @param <E> the type of elements in the node
+ */
+class TreeNode<E> {
+    /** The data object stored in this TreeNode<E>. May be null. */
+    private E data;
+
+    /** Reference to the left subtree. May be null. */
+    private TreeNode<E> left;
+
+    /** Reference to the right subtree. May be null. */
+    private TreeNode<E> right;
+
+    /**
+     * Initialises a new TreeNode<E> using the parameter values.
+     *
+     * @param data reference to the data object to store in this TreeNode<E>
+     * @param left reference to the left subtree
+     * @param right reference to the right subtree
+     */
+    public TreeNode(E data, TreeNode<E> left, TreeNode<E> right) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+
+    /**
+     * Initialises a new TreeNode<E> that has no left subtree and no right
+     * subtree (a "leaf node").
+     *
+     * @param data reference to the data object to store in this TreeNode<E>
+     */
+    public TreeNode(E data) {
+        this(data, null, null);
+    }
+
+    /**
+     * Prints this TreeNode<E> and its subtrees on the screen using
+     * recursive pre-order traversal.
+     */
+    public void printTreePreOrder() {
+        System.out.println(this.data);
+        if (this.left != null) {
+            this.left.printTreePreOrder();
+        }
+        if (this.right != null) {
+            this.right.printTreePreOrder();
+        }
+    }
+
+    /**
+     * Prints this TreeNode<E> and its subtrees on the screen using
+     * recursive in-order traversal.
+     */
+    public void printTreeInOrder() {
+        if (this.left != null) {
+            this.left.printTreeInOrder();
+        }
+        System.out.println(this.data);
+        if (this.right != null) {
+            this.right.printTreeInOrder();
+        }
+    }
+
+    /**
+     * Prints this TreeNode<E> and its subtrees on the screen using
+     * recursive post-order traversal.
+     */
+    public void printTreePostOrder() {
+        if (this.left != null) {
+            this.left.printTreePostOrder();
+        }
+        if (this.right != null) {
+            this.right.printTreePostOrder();
+        }
+        System.out.println(this.data);
+    }
+
+    /**
+     * Prints this TreeNode<E> and its subtrees on the screen using
+     * iterative pre-order traversal with an explicit stack.
+     */
+    public void printTreePreOrderLoop() {
+        Deque<TreeNode<E>> stack = new LinkedList<>();
+        stack.push(this); // initiate the process: push this node on the stack
+        while (!stack.isEmpty()) { // as long as nodes are left to visit...
+            TreeNode<E> node = stack.pop(); // pop the next node for processing
+            System.out.println(node.data); // do the task for the popped node
+            // first push the child node on the stack that is to be visited /later/
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            // then push the child node on the stack that is to be visited /earlier/
+            // (top-of-stack is popped next)
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+    }
+}
